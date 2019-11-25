e41c12395292e2424f57e3e02ca490f613614cfc
cweijiaweil-github <57512674+cweijiaweil-github@users.noreply.github.com>
2019-11-25 15:53:05 +0800

    Update conversation-serv.js

--- services/conversation-serv.js
+++ services/conversation-serv.js
@@ -1,3 +1,45 @@
+const AssistantV2 = require('ibm-watson/assistant/v2');
+const { IamAuthenticator } = require('ibm-watson/auth');
+var constants = require('../utils/constants.json');
+
+const assistant = new AssistantV2({
+  version: CONVERSATION_VERSION,
+  authenticator: new IamAuthenticator({
+    apikey: CONVERSATION_APIKEY,
+  }),
+  url: CONVERSATION_URL,
+});
+
+exports.getConversation = function(pContext) {
+    return new Promise(function (resolve, reject) {
+        var input = pContext.input;
+        var payload = {
+            context: input.context,
+            input: {
+                text: input.question
+            }
+        };
+        assistant.message(payload, function(err, data) {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                var input = pContext.input;
+                input.context = data.context;
+                if (data.output.next_step === constants.conversation_step.answer) {
+
+                } else if (data.output.next_step === constants.conversation_step.contact) {
+                    pContext.qa = data.output.go_qa;
+                } else {
+                    input.answer = data.output.text.join();
+                    input.source = '1';
+                }
+                resolve(pContext);
+            }
+        });
+    });
+};
+
 var Conversation = require('watson-developer-cloud/conversation/v1');
 var vcapServices = require('vcap_services');
 var constants = require('../utils/constants.json');
